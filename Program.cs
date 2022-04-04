ce38a543c8b7ab3ceaa15011e8d982f49f39e339
chakravarthy <m.chakra473@gmail.com>
2022-04-04 18:23:24 +0530

    initiated

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day15_PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day15 Practice Problems!");
+        }
+    }
+}
