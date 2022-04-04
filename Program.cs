b7a27952a3cea1af5e1d5a11a80385fe9214fec4
chakravarthy <m.chakra473@gmail.com>
2022-04-04 18:27:15 +0530

    added [FindFrequency file] to uc1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace Day15_PracticeProblem
+namespace HashTableProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Day15 Practice Problems!");
+            Console.WriteLine("Hello, welcome to HashTable Problem");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value is: " + hash5);
         }
     }
 }
